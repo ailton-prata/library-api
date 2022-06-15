1b0ac5f2c5937464be0ef04c018db027541fae26
Ailton Prata Júnior <junio.kade@gmail.com>
2022-06-15 17:44:25 -0300

    Created book consult design by parameters

--- src/main/java/com/ailtonpratajr/libraryapi/service/BookService.java
+++ src/main/java/com/ailtonpratajr/libraryapi/service/BookService.java
@@ -2,7 +2,9 @@ package com.ailtonpratajr.libraryapi.service;
 
 
 import com.ailtonpratajr.libraryapi.model.entity.Book;
+import org.springframework.data.domain.Page;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface BookService {
@@ -14,4 +16,6 @@ public interface BookService {
     void delete(Book book);
 
     Book update(Book book);
+
+    Page<Book> find(Book filter, Pageable pageRequest);
 }
