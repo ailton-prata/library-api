18d58f43abff05e5048837a2a7461b44cba9a420
Ailton Prata Júnior <junio.kade@gmail.com>
2022-06-22 17:48:05 -0300

    First loan api test

---
+++ src/main/java/com/ailtonpratajr/libraryapi/api/resource/LoanController.java
@@ -0,0 +1,4 @@
+package com.ailtonpratajr.libraryapi.api.resource;
+
+public class LoanController {
+}
---
+++ src/main/java/com/ailtonpratajr/libraryapi/service/LoanService.java
@@ -0,0 +1,7 @@
+package com.ailtonpratajr.libraryapi.service;
+
+import com.ailtonpratajr.libraryapi.model.entity.Loan;
+
+public interface LoanService {
+    Loan save(Loan loan);
+}
