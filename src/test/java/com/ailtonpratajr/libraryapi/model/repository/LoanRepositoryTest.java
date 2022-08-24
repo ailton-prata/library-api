5230439e4920862c9bb982feb32ffdd6365ac2e0
Ailton Prata Júnior <junio.kade@gmail.com>
2022-08-24 18:57:41 -0300

    Finalizing integration loan tests

--- src/test/java/com/ailtonpratajr/libraryapi/model/repository/LoanRepositoryTest.java
+++ src/test/java/com/ailtonpratajr/libraryapi/model/repository/LoanRepositoryTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -33,16 +35,40 @@ public class LoanRepositoryTest {
     public void existsByBookAndNotReturnedTest(){
 
         //cenário
-        Book book = createNewBook("123");
-        entityManager.persist(book);
-
-        Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
-        entityManager.persist(loan);
+        Loan loan = createAndPersistLoan();
+        Book book = loan.getBook();
 
         //execucação
         boolean exists = repository.existsByBookAndNotReturned(book);
 
         assertThat(exists).isTrue();
 
+    }
+
+    @Test
+    @DisplayName("Deve buscar empréstimo pelo isbn do livro ou customer")
+    public void findByBookIsbnOrCustomerTest() {
+
+        //cenário
+        Loan loan = createAndPersistLoan();
+
+        Page<Loan> result = repository.findByBookIsbnOrCustomer("123", "Fulano", PageRequest.of(0, 10));
+
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).contains(loan);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
+
+    }
+
+    public Loan createAndPersistLoan(){
+        Book book = createNewBook("123");
+        entityManager.persist(book);
+
+        Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
+        entityManager.persist(loan);
+
+        return loan;
     }
 }
