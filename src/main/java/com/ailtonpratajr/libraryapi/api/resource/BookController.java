19dba82588c24ee09eec849fbb02ac5b866d6b01
Ailton Prata Júnior <junio.kade@gmail.com>
2022-08-26 18:19:39 -0300

    Implementing route with sub-resource

--- src/main/java/com/ailtonpratajr/libraryapi/api/resource/BookController.java
+++ src/main/java/com/ailtonpratajr/libraryapi/api/resource/BookController.java
@@ -1,10 +1,14 @@
 package com.ailtonpratajr.libraryapi.api.resource;
 
 import com.ailtonpratajr.libraryapi.api.dto.BookDTO;
+import com.ailtonpratajr.libraryapi.api.dto.LoanDTO;
 import com.ailtonpratajr.libraryapi.api.exception.ApiErrors;
 import com.ailtonpratajr.libraryapi.exception.BusinessException;
 import com.ailtonpratajr.libraryapi.model.entity.Book;
+import com.ailtonpratajr.libraryapi.model.entity.Loan;
 import com.ailtonpratajr.libraryapi.service.BookService;
+import com.ailtonpratajr.libraryapi.service.LoanService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,15 +25,12 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController {
 
-    private BookService service;
-    private ModelMapper modelMapper;
-
-    public BookController(BookService service, ModelMapper mapper) {
-        this.service = service;
-        this.modelMapper = mapper;
-    }
+    private final BookService service;
+    private final ModelMapper modelMapper;
+    private final LoanService loanService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -79,6 +80,22 @@ public class BookController {
                 .collect(Collectors.toList());
 
         return new PageImpl<BookDTO>(list, pageRequest, result.getTotalElements());
+    }
+
+    @GetMapping("{id}/loans")
+    public Page<LoanDTO> loanByBook(@PathVariable Long id, Pageable pageable) {
+        Book book = service.getId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Page<Loan> result = loanService.getLoansByBook(book, pageable);
+        List<LoanDTO> list = result.getContent()
+                .stream()
+                .map(loan -> {
+                    Book loanBook = loan.getBook();
+                    BookDTO bookDTO = modelMapper.map(loanBook, BookDTO.class);
+                    LoanDTO loanDTO = modelMapper.map(loan, LoanDTO.class);
+                    loanDTO.setBook(bookDTO);
+                    return loanDTO;
+                }).collect(Collectors.toList());
+        return new PageImpl<LoanDTO>(list, pageable, result.getTotalElements());
     }
 
 
--- src/main/java/com/ailtonpratajr/libraryapi/service/LoanService.java
+++ src/main/java/com/ailtonpratajr/libraryapi/service/LoanService.java
@@ -2,6 +2,7 @@ package com.ailtonpratajr.libraryapi.service;
 
 import com.ailtonpratajr.libraryapi.api.dto.LoanFilterDTO;
 import com.ailtonpratajr.libraryapi.api.resource.BookController;
+import com.ailtonpratajr.libraryapi.model.entity.Book;
 import com.ailtonpratajr.libraryapi.model.entity.Loan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,6 @@ public interface LoanService {
     Loan update(Loan loan);
 
     Page<Loan> find(LoanFilterDTO filterDTO, Pageable pageable);
+
+    Page<Loan> getLoansByBook(Book book, Pageable pageable);
 }
