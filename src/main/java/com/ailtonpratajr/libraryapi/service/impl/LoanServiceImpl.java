9006179c72568dd7093749d6d6989c29b9a460af
Ailton Prata Júnior <junio.kade@gmail.com>
2023-06-07 15:58:14 -0300

    Making changes to receive customer email

--- src/main/java/com/ailtonpratajr/libraryapi/service/impl/LoanServiceImpl.java
+++ src/main/java/com/ailtonpratajr/libraryapi/service/impl/LoanServiceImpl.java
@@ -9,11 +9,12 @@ import com.ailtonpratajr.libraryapi.service.LoanService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
+
 
 @Service
 public class LoanServiceImpl implements LoanService {
+
     private LoanRepository repository;
 
     public LoanServiceImpl(LoanRepository repository) {
