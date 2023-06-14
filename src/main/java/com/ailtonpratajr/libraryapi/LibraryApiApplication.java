08e019a117bd700490331e36d6d261b9f64b5234
Ailton Prata Júnior <junio.kade@gmail.com>
2023-06-14 14:40:39 -0300

    Creating the documentation with Swagger

--- src/main/java/com/ailtonpratajr/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/ailtonpratajr/libraryapi/LibraryApiApplication.java
@@ -17,21 +17,10 @@ import java.util.List;
 @EnableScheduling
 public class LibraryApiApplication {
 
-	@Autowired
-	private EmailService emailService;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
-	}
-
-	@Bean
-	public CommandLineRunner runner(){
-		return args -> {
-			List<String> emails = Arrays.asList("junio.kade@gmail.com");
-			emailService.sendMails("Testando serviço de emails", emails);
-			System.out.println("EMAILS ENVIADOS");
-		};
 	}
 
 	public static void main(String[] args) {
