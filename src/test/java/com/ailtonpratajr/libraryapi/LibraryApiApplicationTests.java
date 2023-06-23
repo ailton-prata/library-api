3e68eb80a32387f7b0a9a5ed2662298ae0b7b0ae
Ailton Prata Júnior <junio.kade@gmail.com>
2023-06-23 12:29:33 -0300

    Correcting a problem in LibraryApiApplicationTests

--- src/test/java/com/ailtonpratajr/libraryapi/LibraryApiApplicationTests.java
+++ src/test/java/com/ailtonpratajr/libraryapi/LibraryApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.ailtonpratajr.libraryapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryApiApplicationTests.class)
 class LibraryApiApplicationTests {
 
 	@Test
